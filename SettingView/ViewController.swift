b5ce65e4bb0527db93640b625e72d52d31b88938
soyung sim <simsy1024@gmail.com>
2024-06-03 14:43:09 +0900

    Initial Commit

---
+++ SettingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingView
+//
+//  Created by 심소영 on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
