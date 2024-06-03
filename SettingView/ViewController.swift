e45c8bc9caa97b8da0c57b0a931bb1d4a460f2d3
soyung sim <simsy1024@gmail.com>
2024-06-03 15:52:29 +0900

    enum, switch, CaseIterable

--- SettingView/ViewController.swift
+++ SettingView/ViewController.swift
@@ -7,13 +7,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+enum SettingCell: Int, CaseIterable{
+    case total, personal, others
+    
+    var mainCell: String {
+        switch self {
+        case .total: return "전체 설정"
+        case .personal: return "개인 설정"
+        case .others: return "기타"
+        }
     }
-
-
+    
+    var subCell: [String] {
+        switch self {
+        case .total: return ["공지사항", "실험실", "버전 정보"]
+        case .personal: return ["개인/보안","알림", "채팅", "멀티프로필"]
+        case .others: return ["고객센터/도움말"]
+        }
+    }
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingTableView()
+        navigationItem.title = "설정"
+    }
+    
+    func settingTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        let xib = UINib(nibName: SettingTableViewCell.identifier, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: SettingTableViewCell.identifier)
+        tableView.rowHeight = 50
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingCell.allCases[section].subCell.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SettingCell.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as! SettingTableViewCell
+        cell.textLabel?.text = SettingCell.allCases[indexPath.section].subCell[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return SettingCell.allCases[section].mainCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+}
+
+protocol IdProtocol: AnyObject {
+    static var identifier: String { get }
+}
+
+extension UITableViewCell: IdProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
